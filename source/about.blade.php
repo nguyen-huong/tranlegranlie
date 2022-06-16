e833b0bfd53769a7ac864a609ab9647d071d773d
nguyen-huong <mhuong1703@gmail.com>
2022-06-15 20:17:16 -0700

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -10,7 +10,5 @@
     <h2>Links:</h2>
 
     <ul>
-        <li><a href="/twitter" target="_blank">Twitter</a></li>
-        <li><a href="/github" target="_blank">GitHub</a></li>
     </ul>
 @endsection
